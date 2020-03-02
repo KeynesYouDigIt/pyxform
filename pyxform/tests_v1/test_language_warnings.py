344d53b8a598104551c3b7e92200e6a86e9e5fbf
vince <vince.buscarello@gmail.com>
2020-03-01 17:47:39 -0700

    test touch ups

--- pyxform/tests_v1/test_language_warnings.py
+++ pyxform/tests_v1/test_language_warnings.py
@@ -13,24 +13,6 @@ class LanguageWarningTest(PyxformTestCase):
     Test language warnings.
     """
 
-    def test_reprod_issue_355(self):
-        survey = self.md_to_pyxform_survey(
-            """
-            | survey |                    |         |                     |
-            |        | type               | name    | label::English (en) |choice-filter
-            |        | select_one country | country | Country             |
-            |        | select_one region  | region  | Region              | pais=$\{country\}
-            """
-        )
-
-        warnings = []
-        tmp = tempfile.NamedTemporaryFile(suffix=".xml", delete=False)
-        tmp.close()
-        survey.print_xform_to_file(tmp.name, warnings=warnings)
-
-        self.assertTrue(len(warnings) == 0)
-        os.unlink(tmp.name)
-
     def test_label_with_valid_subtag_should_not_warn(self):
         survey = self.md_to_pyxform_survey(
             """
@@ -90,6 +72,31 @@ class LanguageWarningTest(PyxformTestCase):
         )
         os.unlink(tmp.name)
 
+    def test_reprod_issue_355(self):
+        survey = self.md_to_pyxform_survey(
+            """
+            | survey  |                    |          |                     |                    |
+            |         | type               | name     | label::English (en) | choice-filter      |
+            |         | select_one country | country  | Country             |                    |
+            |         | select_one city    | city     | City                | country=${country} |
+            | choices |                    |          |                     |                    |
+            |         | list_name          | name     | label               |                    |
+            |         | country            | france   | France              |                    |
+            |         | country            | canada   | Canada              |                    |
+            |         | city               | grenoble | Grenoble            |                    |
+            |         | city               | quebec   | Quebec              |                    |
+            """
+        )
+
+        warnings = []
+        tmp = tempfile.NamedTemporaryFile(suffix=".xml", delete=False)
+        tmp.close()
+        survey.print_xform_to_file(tmp.name, warnings=warnings)
+
+        self.assertTrue(len(warnings) == 0)
+        os.unlink(tmp.name)
+
+
     def test_missing_translation_no_default_lang_media_has_no_language(self):
         # form should test media tag w NO default language set.
         survey = self.md_to_pyxform_survey(
@@ -97,7 +104,29 @@ class LanguageWarningTest(PyxformTestCase):
             | survey  |                 |         |                     |             |
             |         | type            | name    | label::English (en) | media::image|
             |         | integer         | nums    | How many nums?      | opt1.jpg    |
-        """
+            """
+        )
+
+        warnings = []
+        tmp = tempfile.NamedTemporaryFile(suffix=".xml", delete=False)
+        tmp.close()
+        survey.print_xform_to_file(tmp.name, warnings=warnings)
+
+        # In this survey, when 'default' s selected the label of this question will be '-'.
+        # Also, when 'English (en)` is selected, the medial will be '-'
+        self.assertTrue(len(warnings) == 2)
+        os.unlink(tmp.name)
+
+
+    
+    def test_default_language_clears_warning(self):
+        # form should test media tag w NO default language set.
+        survey = self.md_to_pyxform_survey(
+            """
+            | survey  |                 |         |                     |             |
+            |         | type            | name    | label::English (en) | media::image|
+            |         | integer         | nums    | How many nums?      | opt1.jpg    |
+            """
         )
 
         warnings = []
@@ -116,7 +145,7 @@ class LanguageWarningTest(PyxformTestCase):
             | survey  |                 |         |                     |                    |                          |
             |         | type            | name    | label::English (en) | label::French (fr) | media::image::French (fr)|
             |         | integer         | nums    | How many nums?      | Combien noms?      | opt1.jpg                 |
-        """
+            """
         )
 
         warnings = []
@@ -136,7 +165,7 @@ class LanguageWarningTest(PyxformTestCase):
             | survey  |                 |         |                     |                    |                   |
             |         | type            | name    | label::English (en) | label::French (fr) | hint::French (fr) |
             |         | integer         | nums    | How many nums?      | Combien noms?      | noms est nombres  |
-        """
+            """
         )
 
         warnings = []
@@ -148,25 +177,4 @@ class LanguageWarningTest(PyxformTestCase):
             "Question missing translation: /pyxform_autotestname/nums", warnings[0]
         )
         self.assertIn("Column missing: hint", warnings[0])
-        os.unlink(tmp.name)
-
-    def test_default_language_only_should_not_warn(self):
-        survey = self.md_to_pyxform_survey(
-            """
-            | survey |                 |         |        |               |
-            |        | type            | name    | label  | choice_filter |
-            |        | select_one opts | opt     | My opt | fake = 1      |
-            | choices|                 |         |        |               |
-            |        | list_name       | name    | label  | fake          |
-            |        | opts            | opt1    | Opt1   | 1             |
-            |        | opts            | opt2    | Opt2   | 1             |
-        """
-        )
-
-        warnings = []
-        tmp = tempfile.NamedTemporaryFile(suffix=".xml", delete=False)
-        tmp.close()
-        survey.print_xform_to_file(tmp.name, warnings=warnings)
-
-        self.assertTrue(len(warnings) == 0)
         os.unlink(tmp.name)
